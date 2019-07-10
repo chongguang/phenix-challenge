6e8d4a0e537a16701ee4f061c97d79674fa86eb2
Chongguang LIU <lcg31439@gmail.com>
2019-07-10 18:16:40 +0200

    Agg for each shop (#13)
    * generate all files
    
    * clean code

--- solution/src/main/scala/com/carrefour/App.scala
+++ solution/src/main/scala/com/carrefour/App.scala
@@ -1,31 +1,15 @@
 package com.carrefour
 
-import Transaction.getTransactions
-import Utils.{last7Days, top100Revenu, top100Sales, writeToFile}
+
+import Utils.generateResultFiles
 
 object App {
 
   def main(args: Array[String]): Unit = {
     val dayJ: String = "20170514"
-    val dateJ: List[String] = List[String](dayJ)
-    val last7Dates: List[String] = last7Days(dayJ)
-    val transactionsDayJ: List[Transaction] = getTransactions(dateJ)
-    val transactions7Days: List[Transaction] = getTransactions(last7Dates)
-    //transactions.take(10).foreach(println)
-    val top100SalersDayJ = top100Sales(transactionsDayJ)
-    writeToFile(s"output/top_100_ventes_GLOBAL_$dayJ.data", top100SalersDayJ)
-    val top100RevenuProductsDayJ = top100Revenu(transactionsDayJ)
-    writeToFile(s"output/top_100_ca_GLOBAL_$dayJ.data", top100RevenuProductsDayJ)
 
-    val top100Salers7Days = top100Sales(transactions7Days)
-    writeToFile(s"output/top_100_ventes_GLOBAL_$dayJ-J7.data", top100Salers7Days)
-    val top100RevenuProducts7Days = top100Revenu(transactions7Days)
-    writeToFile(s"output/top_100_ca_GLOBAL_$dayJ-J7.data", top100RevenuProducts7Days)
+    generateResultFiles(dayJ)
+    generateResultFiles(dayJ, for7Days = true)
   }
-
-
-
-
-
 
 }
--- solution/src/main/scala/com/carrefour/Utils.scala
+++ solution/src/main/scala/com/carrefour/Utils.scala
@@ -4,6 +4,7 @@ import java.io.{BufferedWriter, FileWriter}
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+import com.carrefour.Transaction.getTransactions
 
 object Utils {
 
@@ -23,6 +24,13 @@ object Utils {
         else acc + (t.produit -> t.qte)
     }.toList.sortBy(-_._2).take(100)
 
+  def top100Revenu(transactions: List[Transaction]): List[(Long, Double)] =
+    transactions.foldLeft(Map[Long, Double]()) {
+      (acc: Map[Long, Double], t: Transaction) =>
+        if (acc.contains(t.produit)) acc + (t.produit -> (acc(t.produit) + t.qte * t.price))
+        else acc + (t.produit -> t.qte * t.price)
+    }.toList.sortBy(-_._2).take(100)
+
   def writeToFile(filename: String, l: List[(Long, Any)]): Unit = {
     val writer = new BufferedWriter(new FileWriter(filename))
     for(tuple <- l) {
@@ -31,11 +39,19 @@ object Utils {
     writer.close()
   }
 
-  def top100Revenu(transactions: List[Transaction]): List[(Long, Double)] =
-    transactions.foldLeft(Map[Long, Double]()) {
-      (acc: Map[Long, Double], t: Transaction) =>
-        if (acc.contains(t.produit)) acc + (t.produit -> (acc(t.produit) + t.qte * t.price))
-        else acc + (t.produit -> t.qte * t.price)
-    }.toList.sortBy(-_._2).take(100)
+  def generateResultFiles(date: String, for7Days: Boolean = false): Unit = {
+    val dates: List[String] = if (for7Days) last7Days(date) else List(date)
+    val transactions: List[Transaction] = getTransactions(dates)
+
+    val dateLabel: String = if (for7Days) date + "-J7" else date
+    writeToFile(s"output/top_100_ventes_GLOBAL_$dateLabel.data", top100Sales(transactions))
+    writeToFile(s"output/top_100_ca_GLOBAL_$dateLabel.data", top100Revenu(transactions))
+
+    Shop.ShopIds.foreach{ s =>
+      val trans = transactions.filter(t => t.shopId == s)
+      writeToFile(s"output/top_100_ventes_${s}_$dateLabel.data", top100Sales(trans))
+      writeToFile(s"output/top_100_ca_${s}_$dateLabel.data", top100Revenu(trans))
+    }
+  }
 
 }
